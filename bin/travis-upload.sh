c4d2c36c084bb2292c0af05a4cfa69a89f24a2fe
hekaisheng <kaisheng.hks@alibaba-inc.com>
2018-12-18 23:12:06 +0800

    test travis upload

--- bin/travis-upload.sh
+++ bin/travis-upload.sh
@@ -9,15 +9,15 @@ if [ "$TRAVIS_TAG" ]; then
   echo "username=pyodps"                             >> ~/.pypirc
   echo "password=$PASSWD"                            >> ~/.pypirc
 
-  python -m pip install twine auditwheel
+  python -m pip install twine
 
   python setup.py bdist_wheel
 
-  for whl in dist/*.whl; do
-    auditwheel show $whl
-	auditwheel repair $whl -w dist/
-  done
-  rm dist/*-linux*.whl
+#  for whl in dist/*.whl; do
+#    auditwheel show $whl
+#	auditwheel repair $whl -w dist/
+#  done
+#  rm dist/*-linux*.whl
 
   python -m twine upload -r pypi --skip-existing dist/*.whl;
 else
